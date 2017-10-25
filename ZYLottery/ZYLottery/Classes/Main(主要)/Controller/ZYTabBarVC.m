174078e74e3f5ff3b3abbad6df2e88f6a3a38a7a
ZY <ZY@163.com>
2017-10-25 18:44:04 +0800

    ZYTabBarVC add ChildViewController

--- ZYLottery/ZYLottery/Classes/Main(主要)/Controller/ZYTabBarVC.m
+++ ZYLottery/ZYLottery/Classes/Main(主要)/Controller/ZYTabBarVC.m
@@ -33,28 +33,44 @@
     //1. 购彩大厅
     ZYHallTableViewController *hallVC = [[ZYHallTableViewController alloc] init];
     hallVC.view.backgroundColor = [UIColor redColor];
-    [self addChildViewController:hallVC];
+    [self setupOneChildViewController:hallVC image:[UIImage imageNamed:@"TabBar_Hall_new"] selectImage:[UIImage imageNamed:@"TabBar_Hall_selected_new"]];
+    
     
     //2. 竞技场
     ZYArenaViewController *arenaVC = [[ZYArenaViewController alloc] init];
     arenaVC.view.backgroundColor = [UIColor greenColor];
     [self addChildViewController:arenaVC];
+    [self setupOneChildViewController:arenaVC image:[UIImage imageNamed:@"TabBar_Arena_new"] selectImage:[UIImage imageNamed:@"TabBar_Arena_selected_new"]];
     
     //3. 发现
     ZYDiscoverTableTableViewController *discoverVC = [[ZYDiscoverTableTableViewController alloc] init];
     discoverVC.view.backgroundColor = [UIColor blueColor];
     [self addChildViewController:discoverVC];
+    [self setupOneChildViewController:discoverVC image:[UIImage imageNamed:@"TabBar_Discovery_new"] selectImage:[UIImage imageNamed:@"TabBar_Discover_selected_new"]];
     
     //4. 开奖信息
     ZYHistoryTableViewController *historyVC = [[ZYHistoryTableViewController alloc] init];
     historyVC.view.backgroundColor = [UIColor yellowColor];
     [self addChildViewController:historyVC];
+    [self setupOneChildViewController:historyVC image:[UIImage imageNamed:@"TabBar_History_new"] selectImage:[UIImage imageNamed:@"TabBar_History_selected_new"]];
     
     //5. 我的彩票
-    ZYMyLotteryViewController *myLotterVC = [[ZYMyLotteryViewController alloc] init];
-    myLotterVC.view.backgroundColor = [UIColor purpleColor];
-    [self addChildViewController:myLotterVC];
+    ZYMyLotteryViewController *myLotteryVC = [[ZYMyLotteryViewController alloc] init];
+    myLotteryVC.view.backgroundColor = [UIColor purpleColor];
+    [self addChildViewController:myLotteryVC];
+    [self setupOneChildViewController:myLotteryVC image:[UIImage imageNamed:@"TabBar_MyLottery_new"] selectImage:[UIImage imageNamed:@"TabBar_MyLottery_selected_new"]];
     
+}
+
+// 添加一个子控制器
+- (void)setupOneChildViewController:(UIViewController *)vc image:(UIImage *)image selectImage:(UIImage *)selectImage
+{
+    [self addChildViewController:vc];
+    vc.tabBarItem.image = image;
+    vc.tabBarItem.selectedImage = selectImage;
+    // 取消图片的渲染
+    vc.tabBarItem.image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    vc.tabBarItem.selectedImage = [selectImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 }
 
 @end
