07ef4638a4755bfed6d4ab05eabc3598c6f46ad3
ZY <ZY@163.com>
2017-10-25 17:24:34 +0800

    add

---
+++ ZYLottery/ZYLottery/Classes/Arena(竞技场)/Controller/ZYTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYTestViewController.h
+//  ZYLottery
+//
+//  Created by 朝阳 on 2017/10/25.
+//  Copyright © 2017年 sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYTestViewController : UIViewController
+
+@end
---
+++ ZYLottery/ZYLottery/Classes/Arena(竞技场)/Controller/ZYTestViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZYTestViewController.m
+//  ZYLottery
+//
+//  Created by 朝阳 on 2017/10/25.
+//  Copyright © 2017年 sunny. All rights reserved.
+//
+
+#import "ZYTestViewController.h"
+
+@interface ZYTestViewController ()
+
+@end
+
+@implementation ZYTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
